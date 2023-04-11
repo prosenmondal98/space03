dc2d7d40e1bd95698dc20bcb0a8bc1ba8904dfb3
prosenjit mondal <122722707+prosenmondal98@users.noreply.github.com>
2023-04-11 22:41:57 +0530

    condition

--- hunger.py
+++ hunger.py
@@ -1,5 +1,7 @@
 print('hellp')
 print('inside github')
 hun= input('are you hungry')
-if hun=='yes' or hun=='no':
-  print('yes i am hungry')
+n=2
+if n<10:
+  if hun=='yes' or hun=='no':
+    print('yes i am hungry')
